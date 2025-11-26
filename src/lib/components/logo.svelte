cd4f8fb9164c258e548ba5d77c56373544c512c8
Cristhian Melo <rxtsel@outlook.com>
2025-11-26 13:56:15 -0500

    feat(ui): add new components

---
+++ src/lib/components/logo.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import { cn } from "@/lib/utils";
+  import type { HTMLImgAttributes } from "svelte/elements";
+
+  let { class: className, ...rest }: HTMLImgAttributes = $props();
+</script>
+
+<img
+  alt="logo"
+  src="/icon-512.png"
+  class={cn("size-13 rounded-xl", className)}
+  {...rest}
+/>
