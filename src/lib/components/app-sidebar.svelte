291300a4beed8e09dca7dc1865376c82135b6a52
Cristhian Melo <rxtsel@outlook.com>
2025-12-14 11:52:09 -0500

    style: add max-width to sidebar

--- src/lib/components/app-sidebar.svelte
+++ src/lib/components/app-sidebar.svelte
@@ -209,7 +209,7 @@
         </Button>
       </div>
     </Sidebar.Header>
-    <Sidebar.Content>
+    <Sidebar.Content class="overflow-y-auto overflow-x-hidden max-w-[400px]">
       <Sidebar.Group class="px-0">
         <Sidebar.GroupContent>
           {#if mails.length === 0 && !isRefreshing}
